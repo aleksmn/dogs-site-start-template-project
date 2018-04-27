17f1ebe2473b197f4ba5073a19e36912c9525656
Mike <al-mn@ya.ru>
2018-04-27 23:23:50 +0300

    humburger icon and menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,26 @@
 $(function() {
 
-	// Custom JS
+
+	// Документация по mmenu http://mmenu.frebsite.nl/documentation/extensions/effects.html
+
+	$('#my-menu').mmenu({
+		extensions: ['theme-black', 'fx-menu-slide', 'pagedim-black', "position-right"],
+		navbar: {
+			title: '<img src="img/logo.png" alt="Сайт о собаках" width="28">'
+		},
+	});
+
+	var API = $('#my-menu').data('mmenu');
+	API.bind('open:finish', function(){
+		$('.hamburger').addClass('is-active');
+	})
+	
+	
+	.bind('close:finish', function(){
+		$('.hamburger').removeClass('is-active');
+	});
+
+
+
 
 });
